fb149a8afa8a240a121937a5ac7feb40dd709aef
Meldrid Cespedes <cespedesmeldrid95@gmail.com>
2022-07-25 23:21:17 -0500

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react"
+import { AddCategory } from "./componentes/AddCategory";
+import { GifGrid } from "./componentes/GifGrid";
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Naruto Shippuden']);
+
+
+
+    return(
+        <>
+            <h2>GifExpertApp</h2>  
+            <AddCategory setCategories={setCategories} /> 
+            <hr />       
+
+            <ol>
+                {
+                    categories.map(category => (
+                        <GifGrid
+                            key={category} 
+                            category={category} 
+                        />
+                    ))
+                }
+            </ol>
+            
+        </>    
+    )
+}
